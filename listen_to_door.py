be5f95c4a35d6213e219fd90e44e98e988e5ee12
AlikaU <alika.utepova@gmail.com>
2026-03-04 20:59:29 -0500

    initial commit

---
+++ listen_to_door.py
@@ -0,0 +1,9 @@
+# serial_bridge.py
+import serial, socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+ser = serial.Serial('/dev/ttyACM0', 9600)
+
+while True:
+    line = ser.readline().decode().strip()
+    sock.sendto(line.encode(), ('127.0.0.1', 4242))
+    print(line)
