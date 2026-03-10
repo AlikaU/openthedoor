ea44cdf66e4d7445477fa101bbe7c857d1e47e04
AlikaU <alika.utepova@gmail.com>
2026-03-09 21:10:31 -0400

    add lilypad port

--- listen_to_door.py
+++ listen_to_door.py
@@ -1,7 +1,8 @@
 # serial_bridge.py
 import serial, socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-ser = serial.Serial('/dev/ttyACM0', 9600)
+# ser = serial.Serial('/dev/ttyACM0', 9600) # arduino
+ser = serial.Serial('/dev/ttyUSB0', 9600) # lilypad
 
 while True:
     line = ser.readline().decode().strip()
